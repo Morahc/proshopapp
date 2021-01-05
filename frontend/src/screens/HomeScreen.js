8fe892343c7f9019b7d9b3fc0b6202f22b0fa5e9
Morah Stephen <55032812+Morahc@users.noreply.github.com>
2021-01-05 17:52:01 +0100

    Update HomeScreen.js

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -23,7 +23,7 @@ const HomeScreen = ({ match }) => {
   return (
     <>
       {!keyword && <ProductCarousel/>} 
-      <h1>Latest Product</h1>
+      <h1>Latest Products</h1>
       {loading ? (
         <Loader />
       ) : error ? (
